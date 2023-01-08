e227d4844aa756e1cae86e3f811c4ecfab90894d
chris-wamae <chriswamae123@gmail.com>
2023-01-08 12:10:08 +0300

    Added  project description

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//added a comment
+// Create a program using OO that takes an input of basic salary as well as benefits and 
+// returns the net salary after decucting payments for NSSF, NHIF as well as PAYE. It also 
+// factors in personal relief.
+//NSSF payment
+//6% upto a maximum of 1080
+//NHIF payment
+//PAYE
+//nssf is deducted before PAYE calculations 
+//personal relief 2.4k
+// 10% upto 24k,
+//there is not PAYE upto 24k as it is below the relief amount
+// 25% from 24k1 to 32.333k,30% above that
